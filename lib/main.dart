c0922d8c959923de17d3f35bac9c81472dda3bb6
Sébastien bellon <sebastien.bellon@nagra.com>
2022-06-22 13:08:13 +0200

    ✨ working with container margin + padding

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            height: 100,
+            width: 100,
+            margin: const EdgeInsets.fromLTRB(30, 10, 50, 20),
+            padding: EdgeInsets.all(20),
+            color: Colors.white,
+            child: const Text("hello"),
+          ),
+        ),
       ),
     );
   }
