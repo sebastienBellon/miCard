fd8133e9d5ec8fa544cbbf910b6f38986aca1219
Sébastien bellon <sebastien.bellon@nagra.com>
2022-06-22 17:40:23 +0200

    ♻️ center column on the middle of the screen + removed padding around avatar

--- lib/main.dart
+++ lib/main.dart
@@ -15,68 +15,67 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(children: <Widget>[
-            const Padding(
-              padding: EdgeInsets.all(15.0),
-              child: CircleAvatar(
-                radius: 50,
-                backgroundColor: Colors.red,
-                backgroundImage: AssetImage('images/avatar.jpg'),
-              ),
-            ),
-            const Text(
-              'Sébastien Bellon',
-              style: TextStyle(
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Pacifico'),
-            ),
-            Text(
-              'Software Developer',
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.teal.shade100,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'SourceSansPro',
-                letterSpacing: 2.5,
-              ),
-            ),
-            Card(
-                margin: const EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.red,
+                  backgroundImage: AssetImage('images/avatar.jpg'),
                 ),
-                child: ListTile(
-                  leading: const Icon(Icons.phone, color: Colors.teal),
-                  title: Text(
-                    "+41 79 755 74 77",
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'SourceSansPro',
-                      fontSize: 20.0,
+                const Text(
+                  'Sébastien Bellon',
+                  style: TextStyle(
+                      fontSize: 40.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Pacifico'),
+                ),
+                Text(
+                  'Software Developer',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.teal.shade100,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'SourceSansPro',
+                    letterSpacing: 2.5,
+                  ),
+                ),
+                Card(
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 10.0,
+                      horizontal: 25.0,
+                    ),
+                    child: ListTile(
+                      leading: const Icon(Icons.phone, color: Colors.teal),
+                      title: Text(
+                        "+41 79 755 74 77",
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    )),
+                Card(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.mail,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'sebastien.bellon@outlook.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                      ),
                     ),
                   ),
-                )),
-            Card(
-              margin: const EdgeInsets.symmetric(
-                vertical: 10.0,
-                horizontal: 25.0,
-              ),
-              child: ListTile(
-                leading: const Icon(
-                  Icons.mail,
-                  color: Colors.teal,
                 ),
-                title: Text(
-                  'sebastien.bellon@outlook.com',
-                  style: TextStyle(
-                    color: Colors.teal.shade900,
-                  ),
-                ),
-              ),
-            ),
-          ]),
+              ]),
         ),
       ),
     );
