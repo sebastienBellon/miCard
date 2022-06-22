ad8795b40ff3067517568114a62bfb268ddac832
Sébastien bellon <sebastien.bellon@nagra.com>
2022-06-22 14:30:28 +0200

    ✨ add some property

--- lib/main.dart
+++ lib/main.dart
@@ -15,26 +15,31 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(children: <Widget>[
-          Container(
-            height: 100,
-            width: 100,
-            color: Colors.white,
-            child: const Text("Container 1"),
-          ),
-          Container(
-            height: 100,
-            width: 100,
-            color: Colors.blue,
-            child: const Text("Container 2"),
-          ),
-          Container(
-            height: 100,
-            width: 100,
-            color: Colors.red,
-            child: const Text("Container 3"),
-          ),
-        ])),
+          child: Column(
+              verticalDirection: VerticalDirection.up,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                  child: const Text("Container 1"),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.blue,
+                  child: const Text("Container 2"),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.red,
+                  child: const Text("Container 3"),
+                ),
+              ]),
+        ),
       ),
     );
   }
