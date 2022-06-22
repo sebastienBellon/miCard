3c6552d0be135190356d78a2c3e18b9d327313c8
Sébastien bellon <sebastien.bellon@nagra.com>
2022-06-22 17:43:58 +0200

    ♻️ add divider line

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,13 @@ class MyApp extends StatelessWidget {
                     letterSpacing: 2.5,
                   ),
                 ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
                 Card(
                     margin: const EdgeInsets.symmetric(
                       vertical: 10.0,
