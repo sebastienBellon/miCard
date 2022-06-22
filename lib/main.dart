cc1170b5cbed682dd1d37cb4436a51d255fe3f69
Sébastien bellon <sebastien.bellon@nagra.com>
2022-06-22 17:22:06 +0200

    ♻️ refactor using card with padding

--- lib/main.dart
+++ lib/main.dart
@@ -39,53 +39,57 @@ class MyApp extends StatelessWidget {
                 letterSpacing: 2.5,
               ),
             ),
-            Container(
+            Card(
               color: Colors.white,
               margin: EdgeInsets.symmetric(
                 vertical: 10.0,
                 horizontal: 25.0,
               ),
-              padding: EdgeInsets.all(10.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(Icons.phone, color: Colors.teal),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    "+41 79 755 74 77",
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'SourceSansPro',
-                      fontSize: 20.0,
+              child: Padding(
+                padding: EdgeInsets.all(25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.phone, color: Colors.teal),
+                    SizedBox(
+                      width: 10.0,
                     ),
-                  )
-                ],
+                    Text(
+                      "+41 79 755 74 77",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-            Container(
+            Card(
               color: Colors.white,
               margin: EdgeInsets.symmetric(
                 vertical: 10.0,
                 horizontal: 25.0,
               ),
-              padding: EdgeInsets.all(10.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.mail,
-                    color: Colors.teal,
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    'sebastien.bellon@outlook.com',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
+              child: Padding(
+                padding: EdgeInsets.all(25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.mail,
+                      color: Colors.teal,
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'sebastien.bellon@outlook.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ]),
